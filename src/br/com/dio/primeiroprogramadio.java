afb6da179051b209d0ae4734d59e5cc99a8e1171
Oliveiraemanuela <oliveiraemanuela1225@gmail.com>
2022-07-18 21:44:29 -0300

    first commit

---
+++ src/br/com/dio/primeiroprogramadio.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprogramadio {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato ();
+		
+		System.out.println(gato);
+	}
+		
+		/*int a = 3;
+		int b = 5;
+		
+		System.out.println("Hello, word!" + (a+b));
+	}*/
+
+}
